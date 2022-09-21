9c0432913a172516d467901abe7f3723c1b7148b
Cosmeak <guillaumefine09@gmail.com>
2022-09-21 16:04:31 +0200

    :bug: FIX server

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,52 +1,81 @@
-export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
+export default {
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
   head: {
-    title: 'jungle-arch',
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
+  /*
+  ** Global CSS
+  */
   css: [
-    './style/app.scss'
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+  ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    // baseURL: '/',
-    proxy: true,
-    host: '/api/',
-    prefix: '/api/',
-  },
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://http.nuxtjs.org
+    '@nuxt/http'
+  ],
 
+  /*
+  ** Server Middleware
+  */
   serverMiddleware: {
-    '/api': '~/api/server.js'
+    '/api': '~/api'
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  /*
+  ** For deployment you might want to edit host and port
+  */
+  // server: {
+  //  port: 8000, // default: 3000
+  //  host: '0.0.0.0' // default: localhost
+  // },
+
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  build: {
+  }
 }
